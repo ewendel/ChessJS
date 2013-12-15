0d92d109f3a8225ddb6feb607cb9db47887711de
Erik Wendel <erik@wendel.no>
2013-12-15 22:06:23 +0100

    moved board to component

--- js/component/board.js
+++ js/component/board.js
@@ -3,14 +3,6 @@ define(function (require) {
     var eventBus = require('component/events');
     var Path = require('component/path');
 
-    eventBus.on('promotion', function(position) {
-    	promotion = position;
-    });
-
-	var state = [];
-
-	var promotion;
-
 	function outOfBounds(col, row) {
 		if (arguments.length === 1) {
 			var position = arguments[0];
@@ -21,72 +13,84 @@ define(function (require) {
 		return false;
 	}
 
-	var Board = {
+	return function() {
+		var self = this;
+		this.state = [[],[],[],[],[],[],[],[]];
+
 		// NEW API
-		clear: function() {
-			state = undefined;
-			state = [];
-			_.times(8, function() { state.push([]); _.times(8, function() { _.last(state).push(undefined); }); })
-		},
-		add: function(piece) {
+		this.clear = function() {
+			self.state = undefined;
+			self.state = [];
+			_.times(8, function() { self.state.push([]); _.times(8, function() { _.last(self.state).push(undefined); }); });
+		};
+
+		this.add = function(piece) {
 			if (!piece) throw "Board:add() needs piece param";
 			if (outOfBounds(piece.position())) throw "Board:add() position out of bounds";
 			var col = piece.position().col;
 			var row = piece.position().row;
-			state[row][col] = piece;
-		},
-		get: function(col, row) {
+			self.state[row][col] = piece;
+		};
+
+		this.get = function(col, row) {
 			if (arguments.length === 1) {
 				var position = Path.convert(col);
 				col = position.col;
 				row = position.row;
 			}
-			return state[row][col];
-		},
-		remove: function(col, row) {
+			return self.state[row][col];
+		};
+
+		this.remove = function(col, row) {
 			if (arguments.length === 1) {
 				var position = Path.convert(col);
 				col = position.col;
 				row = position.row;
 			}
-			state[row][col] = undefined;
-		},
-		pieces: function() {
-			return _.size(_.compact(_.flatten(state)));
-		},
+			self.state[row][col] = undefined;
+		};
+
+		this.pieces = function() {
+			return _.size(_.compact(_.flatten(self.state)));
+		};
 
 		// OLD API
-		init: function(board) {
+		this.init = function(board) {
 			if (!board || !_.isArray(board) || board.length !== 8) {
 				throw "Board was set up with illegal params"
 			}
-			state = board;
-		},
-		getPieceForPosition: function(col, row) {
+			self.state = board;
+		};
+
+		this.getPieceForPosition = function(col, row) {
 			if (arguments.length === 0) throw "getPieceForPosition needs argument";
 			if (outOfBounds(col, row)) return undefined;
-			return state[row][col];
-		},
-		setPosition: function(col, row, piece) {
+			return self.state[row][col];
+		};
+
+		this.setPosition = function(col, row, piece) {
 			if (outOfBounds(col, row)) return undefined;
-			state[row][col] = piece;
-		},
-		positionIsEmpty: function(col, row) {
+			self.state[row][col] = piece;
+		};
+
+		this.positionIsEmpty = function(col, row) {
 			if (outOfBounds(col, row)) return undefined;
-			return state[row][col] === undefined;
-		},
-		getPiecesForPlayer: function(player) {
+			return self.state[row][col] === undefined;
+		};
+
+		this.getPiecesForPlayer = function(player) {
 			var pieces = [];
-			_.each(state, function(row) {
+			_.each(self.state, function(row) {
 				_.each(row, function(piece) {
 					if (piece && piece.id() === player) pieces.push(piece);
 				});
 			})
 			return pieces;
-		},
-		generateMoves: function(player) {
+		};
+
+		this.generateMoves = function(player) {
 			var moves = [];
-			var pieces = Board.getPiecesForPlayer(player);
+			var pieces = self.getPiecesForPlayer(player);
 			_.each(pieces, function(piece) {
 				var validMoves = piece.getValidMoves();
 				_.each(validMoves, function(move) {
@@ -98,16 +102,15 @@ define(function (require) {
 			});
 			if (moves.length === 0) debugger;
 			return moves;
-		},
-		findPromotionCandidate: function() {
-			return promotion ? state[promotion.row][promotion.col] : undefined;
-		},
+		};
 
-		debug: function() {
+
+
+		this.debug = function() {
 			var boardAsString = "";
 			boardAsString += "Chess Board:\n --------------------------\n";
-			_.each(state, function(row, index) {
-				_.each(state[index], function(piece) {
+			_.each(self.state, function(row, index) {
+				_.each(self.state[index], function(piece) {
 					if (!piece) {
 						boardAsString += "   ";
 					} else {
@@ -125,10 +128,7 @@ define(function (require) {
 			});
 			boardAsString += "---------------------------"
 			console.log(boardAsString);
-		}
+		};
 	};
-
-
-	return Board;
 
 });
